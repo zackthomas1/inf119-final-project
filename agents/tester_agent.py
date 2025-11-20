f21a701e960dfebcfe788861e88559140ca4abaa
collinvt <collinvt@uci.edu>
2025-11-19 16:40:35 -0800

    Add header documentation to tester_agent.py
    Added file header with authors and description.

--- agents/tester_agent.py
+++ agents/tester_agent.py
@@ -1,3 +1,13 @@
+"""
+File: tester_agent.py
+Authors: 
+    - [Zachery Thomas] ([47642149])
+    - [Collin Vinh Tran] ([47304556])
+    - [Jenny Thao Ly] ([83605957])
+    - [Lina Nguyen] ([70703520])
+Description: [What this file does (functions/methods used)]
+"""
+
 # TesterAgent generates test cases for the code produced by CoderAgent.
 
 from typing import Dict, Any
